8bbdc2d62da24db25da297a1ed9d2a356251c025
Dima <random1k11@gmail.com>
2019-06-01 19:59:37 +0300

    removed CamelCase style from function names and added functions 'get_last_block' and 'add_block'

--- DARCoin.py
+++ DARCoin.py
@@ -9,13 +9,13 @@ class Block():
         self.timestampt = timestampt
         self.transactions = transactions
         self.prev_hash = prev_hash
-        self.hash = self.calcHash()
+        self.hash = self.calc_hash()
 
-    def calcHash(self):
+    def calc_hash(self):
         block_string = json.dumps({'nonce': self.nonce, 'timestampt': self.timestampt, 'transactions': self.transactions, 'prev_hash': self.prev_hash}, sort_keys=True).encode('utf-8')
         return hashlib.sha256(block_string).hexdigest()
 
-    def printHashes(self):
+    def print_hashes(self):
         print('Предыдущий HASH:', self.prev_hash)
         print('HASH:', self.hash)
 
@@ -23,10 +23,20 @@ class Block():
 class BlockChain():
 
     def __init__(self):
-        self.chain = [self.generateGenesisBlock(),]
+        self.chain = [self.generate_genesis_block(),]
 
-    def generateGenesisBlock(self):
+    def generate_genesis_block(self):
         return Block(0, '01/06/2019', 'Genesis Block')
+
+    def get_last_block():
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.prev_hash = self.get_last_block().hash
+        new_block.hash = new_block.calc_hash()
+        self.chain.append(new_block)
+
+
 
 
 
@@ -34,4 +44,4 @@ class BlockChain():
 
 if __name__ == "__main__":
     block = Block(1, '01/06/2019', 100)
-    block.printHashes()
+    block.print_hashes()
