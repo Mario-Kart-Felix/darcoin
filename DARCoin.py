73df56b9b634219a5b6dd01c4f98db2b4d398a7e
Dima <random1k11@gmail.com>
2019-06-01 23:00:44 +0300

    Created function "validate_chain" for validates th chain

--- DARCoin.py
+++ DARCoin.py
@@ -15,9 +15,14 @@ class Block():
         block_string = json.dumps({'nonce': self.nonce, 'timestampt': self.timestampt, 'transactions': self.transactions, 'prev_hash': self.prev_hash}, sort_keys=True).encode('utf-8')
         return hashlib.sha256(block_string).hexdigest()
 
-    def print_hashes(self):
-        print('Предыдущий HASH:', self.prev_hash)
-        print('HASH:', self.hash)
+    def __str__(self):
+        string = 'nonce: ' + str(self.nonce) + ', \n'
+        string += 'timestampt: ' + str(self.timestampt) + ', \n'
+        string += 'transactions: ' + str(self.transactions) + ', \n'
+        string += 'prev_hash: ' + str(self.prev_hash) + ', \n'
+        string += 'hash: ' + str(self.hash) + ', \n'
+        return string
+
 
 
 class BlockChain():
@@ -28,7 +33,7 @@ class BlockChain():
     def generate_genesis_block(self):
         return Block(0, '01/06/2019', 'Genesis Block')
 
-    def get_last_block():
+    def get_last_block(self):
         return self.chain[-1]
 
     def add_block(self, new_block):
@@ -36,12 +41,27 @@ class BlockChain():
         new_block.hash = new_block.calc_hash()
         self.chain.append(new_block)
 
-
-
+    def validate_chain(self):
+        for i in range(1, len(self.chain)):
+            prev_block = self.chain[i-1]
+            curr_block = self.chain[i]
+            if curr_block.hash != curr_block.calc_hash():
+                print('Не правильный блок', curr_block.prev_hash)
+                return False
+            if curr_block.prev_hash != prev_block.hash:
+                print('Не правильная цепочка')
+                return False
+        return True
 
 
 
 
 if __name__ == "__main__":
-    block = Block(1, '01/06/2019', 100)
-    block.print_hashes()
+    DARCoin = BlockChain()
+    DARCoin.add_block(Block(1, '20/05/2019', 100))
+    DARCoin.add_block(Block(2, '25/05/2019', 50))
+    DARCoin.chain[2].transactions = 333
+    DARCoin.chain[2].hash = DARCoin.chain[2].calc_hash()
+    for b in DARCoin.chain:
+        print(b)
+    print(DARCoin.validate_chain())
